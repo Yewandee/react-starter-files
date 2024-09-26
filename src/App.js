631148d682ad625416f54d1eb96a81e414577398
Joshua Fajobi <Joshua>
2024-09-26 18:46:55 +0100

    project folder structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import RoutesSystem from "./routes/routes";
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import './App.css'
+import { AuthProvider } from "./context/AuthProvider";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <AuthProvider>
+          <Routes>
+            <Route path="/*" element={<RoutesSystem />} />
+          </Routes>
+        </AuthProvider>
+      </Router>
     </div>
   );
 }
