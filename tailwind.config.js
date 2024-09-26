40f36cb1c358a89c0f842440a537c08d0fab9286
Joshua Fajobi <Joshua>
2024-09-26 18:19:14 +0100

    starter file with fontawesome installed

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+      extend: {
+        colors: {
+          'priColor': '#272662',
+          'secColoor': '#f7f7f7'
+        }
+      },
+    },
+    plugins: [],
+  }
+  
+  
